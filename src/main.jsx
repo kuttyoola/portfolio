5ab4b6c1cc7b50419d4e7d87d455ed6f859c043d
Hariharan M <131140205+kuttyoola@users.noreply.github.com>
2025-08-17 11:34:08 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
-import Use from './component/useEffect'
-import Useref from './component/useRef'
-import Usememo from './component/useMemo'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
